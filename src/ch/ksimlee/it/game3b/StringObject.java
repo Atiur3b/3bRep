b15babb58938d8f9e089e7c3246be73ee9a01f4c
Nik <nibur2000@gmail.com>
2014-12-02 10:10:08 +0100

    zuesätzlich

---
+++ src/ch/ksimlee/it/game3b/StringObject.java
@@ -0,0 +1,41 @@
+package ch.ksimlee.it.game3b;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class StringObject extends RenderObject {
+
+	private String content;
+
+	public StringObject(int x, int y, int zIndex, String content) {
+		super(x, y, zIndex, false);
+		this.content = content;
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	public void setContent(String content) {
+		this.content = content;}
+	
+	
+	
+	public void render(Graphics g) {
+		// TODO set color
+		g.setColor(Color.BLACK);
+		g.drawString(content, x, y);
+		
+
+	}
+
+}
